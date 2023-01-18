92a707b40dd31ff9bb2e41d3a5d3cd082c93d931
zachrodis <zachrodis@gmail.com>
2023-01-18 17:23:52 -0500

    more ch 4 exercises

--- 4/exercises.lua
+++ 4/exercises.lua
@@ -6,9 +6,6 @@ luaunit = require('luaunit')
 -- Exercise 4.3
 -- Write a funtion to insert a string into a given position of another one
 -- insert("hello world", 1, "start:")  --> start: hello world
-
--- strategy: use string.sub split the original string in two,
--- then concatenate with the insertion
 function insert(str, pos, insertion)
 
     local firstHalf = string.sub(str, 0, pos - 1)
@@ -23,6 +20,64 @@ function testInsert()
 
     local result2 = insert("hello world", 7, "small ")
     luaunit.assertEquals(result2, "hello small world")
+end
+
+--[[
+function testInsertUTF8()
+    -- fails with non-unicode characters
+    local utf8Result = insert("ação", 5, "!")
+    luaunit.assertEquals(utf8Result, "ação!")
+end
+--]]
+
+-- can't do Exercise 4.4 or 4.6 in Lua 5.2... need to upgrade to 5.3
+-- there is a new library that handles utf8 characters.
+
+--Exercise 4.5
+function remove(str, pos, len)
+    local firstHalf = string.sub(str, 0, pos - 1)
+    local secondHalf = string.sub(str, pos + len)
+
+    return firstHalf .. secondHalf
+end
+
+function testRemove()
+    local result = remove("hello world", 7, 4)
+    luaunit.assertEquals(result, "hello d")
+end
+
+--Exercise 4.7
+function isPalindrome(str)
+    local lowercase = string.lower(str)
+    return lowercase == string.reverse(lowercase)
+end
+
+function testIsPalindrome()
+    local resultTrue = isPalindrome('step on no pets')
+    local resultfalse = isPalindrome('bananas')
+
+    luaunit.assertEquals(resultTrue, true)
+    luaunit.assertEquals(resultfalse, false)
+end
+
+-- Exercise 4.8
+function isPalindromeWithoutPunctuationOrSpaces(str)
+    local removeThese = { ' ', '%.', '%,', '%:', '%;', '%?', '%!', '%-' }
+    local result = str
+    print('- ' .. result)
+    for i, item in pairs(removeThese) do
+        result = string.gsub(result, item, '')
+    end
+
+    return isPalindrome(result)
+end
+
+function testIsPalindromeWithoutPunctuationOrSpaces()
+    local resultTrue = isPalindromeWithoutPunctuationOrSpaces("step. on nopets")
+    local resultFalse = isPalindromeWithoutPunctuationOrSpaces("bananas")
+
+    luaunit.assertEquals(resultTrue, true)
+    luaunit.assertEquals(resultFalse, false)
 end
 
 luaunit.LuaUnit.run()
